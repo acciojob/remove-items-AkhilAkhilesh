25f580e6737373e28c98900f31b48ac2a6e2269c
AkhilAkhilesh <120119311+AkhilAkhilesh@users.noreply.github.com>
2026-04-07 19:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 //your JS code here. If required.
 function removeColor() {
-	const btn = document.querySelector('input[type="button"]');
-	btn.addEventListener("click", () => {
 	  const select = document.getElementById("colorSelect");
-	  if (select.selectedIndex !== -1) {
+	  if (select.options.length > 0) {
 	    select.remove(select.selectedIndex);
 	  }
-});
 }
 
 document.querySelector('input[type="button"]').addEventListener("click", removeColor);
